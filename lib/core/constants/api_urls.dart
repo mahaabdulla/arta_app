26464c0718ad567e86f3fde5f2d881c5077165fb
Hebah-ALZzubaidi <heba.osama@etgan.me>
2025-03-13 00:51:56 +0300

    ff

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const String ipconfig = '192.168.25.107';
+  static const String ipconfig = '192.168.5.121';
   static const String root = 'http://$ipconfig:8000/api';
   static const String getParentUrl = '$root/categories/parents';
   static const String getcategories = '$root/category';
